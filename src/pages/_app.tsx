def9ad3d304a4eb685dc7c61e89168608de12098
Alison Bessa <alisonbessa@gmail.com>
2022-09-12 16:53:29 -0300

    feat: create application header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,21 @@
 import { AppProps } from "next/app"
-import { globalStyles } from "../styles/global";
+import { globalStyles } from "../styles/global"
 
-globalStyles();
+import logoImg from "../assets/logo.svg"
+import { Container, Header } from "../styles/pages/app"
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+globalStyles()
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <Container>
+      <Header>
+        <img src={logoImg.src} alt="" />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  )
 }
+
+export default App
